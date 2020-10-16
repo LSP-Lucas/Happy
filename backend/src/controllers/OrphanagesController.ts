b749ff7156d2107613ffb64de79931cfaa408495
Lucas Pedroso <lucassppedroso@gmail.com>
2020-10-16 20:05:54 -0300

    finalização dos projeto web

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -52,7 +52,7 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends,
+            open_on_weekends: open_on_weekends == 'true',
             images
         }
 
@@ -64,10 +64,11 @@ export default {
             instructions: Yup.string().required(),
             opening_hours: Yup.string().required(),
             open_on_weekends: Yup.boolean().required(),
-            images: Yup.array(Yup.object().shape({
-                path: Yup.string().required()
-            }))
-
+            images: Yup.array(
+                Yup.object().shape({
+                    path: Yup.string().required()
+                })
+            )
         });
 
         await schema.validate(data, {
